1481cf30097469600ec983a7c5be371c789781ec
Sarthak Deore <sarthakdeore2006@gmail.com>
2026-04-20 18:35:42 +0530

    added threads for fast speed

--- projects/port-scanner/scanner.py
+++ projects/port-scanner/scanner.py
@@ -1,4 +1,5 @@
 import socket
+from threading import Thread
 
 target = input("Enter target IP: ")
 start_port = int(input("Enter start port: "))
@@ -6,13 +7,16 @@ end_port = int(input("Enter end port: "))
 
 print(f"\nScanning {target} from port {start_port} to {end_port}\n")
 
-for port in range(start_port, end_port + 1):
+def scan(port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     socket.setdefaulttimeout(1)
 
     result = s.connect_ex((target, port))
-
     if result == 0:
         print(f"Port {port} is OPEN")
 
     s.close()
+
+for port in range(start_port, end_port + 1):
+    t = Thread(target=scan, args=(port,))
+    t.start()
