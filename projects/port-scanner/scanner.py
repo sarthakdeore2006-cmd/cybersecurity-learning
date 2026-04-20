fc7dbe2b2cbdbacb10cfe2532a1ac78d62d9703a
Sarthak Deore <sarthakdeore2006@gmail.com>
2026-04-20 18:23:33 +0530

    Added custom port range scanning feature

--- projects/port-scanner/scanner.py
+++ projects/port-scanner/scanner.py
@@ -1,2 +1,18 @@
+import socket
+
 target = input("Enter target IP: ")
-print("Scanning target:", target)
+start_port = int(input("Enter start port: "))
+end_port = int(input("Enter end port: "))
+
+print(f"\nScanning {target} from port {start_port} to {end_port}\n")
+
+for port in range(start_port, end_port + 1):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socket.setdefaulttimeout(1)
+
+    result = s.connect_ex((target, port))
+
+    if result == 0:
+        print(f"Port {port} is OPEN")
+
+    s.close()
